862a59a3020d3cb9b3beef48f3260cff943f3085
shreyas-codecraft <shreyas.salyan@codecraft.co.in>
2024-08-23 11:05:53 +0530

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ 
+  subsets: ['latin'], 
+  weight: ['400', '700'] // Normal (400) and bold (700) weights
+});
